07d8363760b00f227d4ccabd76a7883678a4febb
aa12 <875151567@qq.com>
2023-11-11 20:46:24 +0800

    chore 清除固定用户账号密码

--- src/view/login/index.vue
+++ src/view/login/index.vue
@@ -149,8 +149,8 @@ loginVerify();
 const loginForm = ref(null);
 const picPath = ref("");
 const loginFormData = reactive({
-  username: "admin",
-  password: "123456",
+  username: "",
+  password: "",
   captcha: "",
   captchaId: "",
   openCaptcha: false,
